c58e7a953c5a9764de57f6875c3c4ca09f17a1d3
Joan Climent <joandelason@gmail.com>
2015-10-30 15:13:31 +0100

    Initial commit
    Initial version with low performance nodes rendering.

---
+++ js/main.js
@@ -0,0 +1,198 @@
+var orbitControls;
+
+$(function() {
+	
+	var stats = initStats();
+	
+	
+	var scene = new THREE.Scene();
+	var camera = new THREE.PerspectiveCamera(45, window.innerWidth/window.innerHeight, 0.1, 1000);
+	var renderer = new THREE.WebGLRenderer();
+	
+	//renderer.setClearColorHex(0xEEEEEE);
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	
+	var axes = new THREE.AxisHelper(20);
+	scene.add(axes);
+		
+	//CONTROLS
+	var controls = new function(){
+		this.red = 255;
+		this.green = 255;
+		this.blue = 255;
+		this.lat = 0;
+		this.lon = 0;
+	};
+	
+	var gui = new dat.GUI();
+	gui.add( controls, 'red', 0, 255);
+	gui.add( controls, 'green', 0, 255);
+	gui.add( controls, 'blue', 0, 255);
+	gui.add( controls, 'lat', -90, 90);
+	gui.add( controls, 'lon', -180, 180);
+	
+	
+	var earthRadius = 28;
+	var sphereGeom   = new THREE.SphereGeometry(earthRadius, 30, 30)
+	var texture = THREE.ImageUtils.loadTexture('images/earthmap1k.jpg', {}, function() {
+						renderer.render(scene);
+					});
+	var sphereMaterial  = new THREE.MeshLambertMaterial({map: texture});
+	var sphere = new THREE.Mesh(sphereGeom, sphereMaterial);
+	
+	sphere.position.x = 0;
+	sphere.position.y = 0;
+	sphere.position.z = 0;
+	
+	
+	scene.add(sphere);
+	
+	camera.position.x = -30;
+	camera.position.y = 40;
+	camera.position.z = 30;
+	camera.lookAt(scene.position);
+	
+	orbitControls = new THREE.OrbitControls(camera);
+	
+	
+	displayNodes();
+	
+	var whiteMat = new THREE.LineBasicMaterial({
+		color: 0xffffff
+	});
+
+	var lineGeom = new THREE.Geometry();
+	lineGeom.vertices.push(
+		new THREE.Vector3( 0, 0, 0 ),
+		new THREE.Vector3( 0, 0, 30 )	
+	);
+	lineGeom.verticesNeedUpdate = true;
+	var line = new THREE.Line( lineGeom, whiteMat );
+	
+	scene.add( line );
+	
+	
+	
+	
+	var blueMat = new THREE.LineBasicMaterial({
+		color: 0x0000ff
+	});
+
+	var xGeom = new THREE.Geometry();
+	xGeom.vertices.push(
+		new THREE.Vector3( -100, 0, 0 ),
+		new THREE.Vector3( 100, 0, 0 )	
+	);
+
+	var xAxis = new THREE.Line( xGeom, blueMat );
+	//scene.add( xAxis );
+	
+	// -------- Y
+	var matGreen = new THREE.LineBasicMaterial({
+		color: 0x00ff00
+	});	
+	
+	var yGeom = new THREE.Geometry();
+	yGeom.vertices.push(
+		new THREE.Vector3( 0, -10, 0 ),
+		new THREE.Vector3( 0, 10, 0 )	
+	);
+	var yAxis = new THREE.Line( yGeom, matGreen );
+	//scene.add( yAxis );
+	
+	// --------- Z
+	var zGeom = new THREE.Geometry();
+	var matRed = new THREE.LineBasicMaterial({
+		color: 0xff0000
+	});
+	zGeom.vertices.push(
+		new THREE.Vector3( 0, 0, -100 ),
+		new THREE.Vector3( 0, 0, 100 )	
+	);
+	
+	
+	var zAxys = new THREE.Line( zGeom, matRed );
+	//scene.add(zAxys);
+	
+	
+	
+	//LIGHT
+	var spotLight = new THREE.SpotLight(0xffffff);
+	spotLight.position.set(-40, 60, -10);
+	scene.add(spotLight);
+	
+	scene.add(new THREE.AmbientLight(0xffffff));
+	
+	
+	$("#webgl").append(renderer.domElement);
+	renderer.render(scene, camera);
+	
+	
+	var dirLight = new THREE.DirectionalLight(0xffffff);
+	dirLight.position.set(10,10,10);
+	scene.add(dirLight);
+	
+	
+	renderScene();
+	
+	
+	function displayNodes(){
+		var nodes = nodesData["nodes"];
+		var whiteMat = new THREE.LineBasicMaterial({
+			color: 0xffffff
+		});
+		
+		var r = earthRadius*1.1;
+		for(var node in nodes){
+			//console.log(node +"-->" +nodes[node]);
+			var lat = nodes[node][8]*Math.PI/180.0;
+			var lon = -nodes[node][9]*Math.PI/180.0;
+			
+			var lineGeom = new THREE.Geometry();
+			lineGeom.vertices.push(
+				new THREE.Vector3( 0, 0, 0 ),
+				new THREE.Vector3( 0, 0, 30 )	
+			);
+			var pinLine = new THREE.Line( lineGeom, whiteMat );
+			
+			pinLine.geometry.vertices[1].x  = r * Math.cos(lat)*Math.cos(lon);
+			pinLine.geometry.vertices[1].y  = r * Math.sin(lat);
+			pinLine.geometry.vertices[1].z  = r * Math.cos(lat)*Math.sin(lon);
+			scene.add( pinLine );
+			
+		}		
+	}
+	
+	function renderScene(){	
+		stats.update();
+		//step += .04;
+		//sphere.position.x = 20 + 10*Math.cos(step);
+		//sphere.position.y = 2 + 10 * Math.abs(Math.sin(step));
+		orbitControls.update();
+				
+		var r = 1.1*earthRadius;
+		line.geometry.vertices[1].x  = r * Math.cos(controls.lat*Math.PI/180.0)*Math.cos(controls.lon*Math.PI/180.0);
+		line.geometry.vertices[1].y  = r * Math.sin(controls.lat*Math.PI/180.0);
+		line.geometry.vertices[1].z  = r * Math.cos(controls.lat*Math.PI/180.0)*Math.sin(controls.lon*Math.PI/180.0);
+		line.geometry.verticesNeedUpdate = true; 
+		
+		
+		sphere.material.color.setRGB(controls.red/255.0, controls.green/255.0, controls.blue/255.);
+		requestAnimationFrame(renderScene);
+		renderer.render(scene, camera);
+		
+	}
+	
+	function initStats(){
+		var stats = new Stats();
+		stats.setMode(0);		
+		
+		stats.domElement.style.position = 'absolute';
+		stats.domElement.style.left = 0;
+		stats.domElement.style.top = 0;
+		
+		document.getElementById("stats").appendChild(stats.domElement);
+		
+		return stats;
+	}
+});
